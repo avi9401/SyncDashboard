fbefa266b41b074be660229e46bccafc0634845d
avi <avikhajuria1994@outlook.com>
2023-07-29 09:42:09 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
- import { HashRouter } from 'react-router-dom';
 import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-     <HashRouter>
       <App />
-     </HashRouter>
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
