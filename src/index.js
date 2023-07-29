b212b3d52af1cffef729700d79bfbe9e26623f96
avi <avikhajuria1994@outlook.com>
2023-07-29 10:11:17 +0530

    routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
@@ -8,7 +8,9 @@ import { ContextProvider } from './contexts/ContextProvider';
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
+     <HashRouter>
       <App />
+     </HashRouter>
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
