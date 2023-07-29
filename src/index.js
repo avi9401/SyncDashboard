bb0178705b542db44da3dbd0d1ff22680bf0121e
avi <avikhajuria1994@outlook.com>
2023-07-29 10:16:58 +0530

    layout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+
 import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-     <HashRouter>
       <App />
-     </HashRouter>
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
